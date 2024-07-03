8d8f5b90c931bb7279d1608b4c67e2df35e42d0c
Jamie Yoo <naive0612@gmail.com>
2024-07-03 15:01:43 +0900

    feat : add Footer
    add Footer

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Router from 'route/router';
+import Footer from 'components/common/layout/Footer';
+import 'assets/styles/Footer.css';
+
+function App() {
+  return (
+    <>
+      <Router />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ src/reportWebVitals.ts
@@ -0,0 +1,32 @@
+// reportWebVitals.js
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
+
+
+// reportWebVitals.ts
+import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
