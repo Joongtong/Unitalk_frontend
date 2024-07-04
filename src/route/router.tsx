af043aca635ca923b241589d2215767dc7f9572e
Jamie Yoo <naive0612@gmail.com>
2024-07-04 18:15:00 +0900

    feat : modify some settings
    readme, app & route, foldering

--- src/route/router.tsx
+++ src/route/router.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//Import Components
+import Header from 'components/common/layout/Header';
+import Footer from 'components/common/layout/Footer';
+
+//Import Css
+import 'assets/styles/common/Header.css';
+import 'assets/styles/common/Footer.css';
+
 function Router() {
     return(
         <BrowserRouter>
-            {/* <Header/> */}
+            <Header/>
             <Routes>
                 
             </Routes>
-            {/* <Footer/> */}
+            <Footer/>
         </BrowserRouter>
     )
 }   
