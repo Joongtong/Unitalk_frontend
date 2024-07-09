eceeaa38a654ecce7df34f8f14ebdfa17a8c8db3
RedbeanCream0 <khj0633@gmail.com>
2024-07-09 23:34:32 +0900

    test_security_login
    test

---
+++ src/pages/login/login.js
@@ -0,0 +1,41 @@
+// login.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:8080/login', {
+                username,
+                password,
+            });
+            localStorage.setItem('token', response.data.token);
+            window.location.href = '/mypage';
+        } catch (error) {
+            console.error('Login failed', error);
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+}
+
+export default Login;
