8956e04bb40fe422f34196299dacf5d7643b8b69
HeegeneChae <heegenechae@gmail.com>
2024-07-15 03:03:17 +0900

    chore: Completed WebSocket Stomp chat message testing

--- src/route/router.tsx
+++ src/route/router.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import EmpHome from 'pages/emp/EmpHome';
+import ChatHome from 'pages/online/ChatHome';
 
 function Router() {
-    return(
+    return (
         <BrowserRouter>
             {/* <Header/> */}
             <Routes>
-                <Route path='/empHome' element={<EmpHome/>} />
+                <Route path='/empHome' element={<EmpHome />} />
+                <Route path="/chathome" element={<ChatHome />} />
             </Routes>
             {/* <Footer/> */}
         </BrowserRouter>
