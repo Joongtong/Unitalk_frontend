f7575c3d247a634afb0b261f2e9229bc96e38d79
Rbcream <khj0633@gmail.com>
2024-07-16 12:31:06 +0900

    repair_merge
    통합부분확인

--- src/route/router.tsx
+++ src/route/router.tsx
@@ -16,20 +16,13 @@ import ManageCounseling from 'pages/emp/ManageCounseling';
 import 'assets/styles/common/Header.css';
 import 'assets/styles/common/Footer.css';
 import 'assets/styles/common/NavigationMenu.css';
-
+// 프로그램 Component
 import ProgramList from 'pages/program/ProgramList';
 import ProgramDetail from 'pages/program/ProgramDetail';
 import ProgramManagement from 'pages/program/ProgramManagement';
 import ProgramForm from 'components/program/ProgramForm';
 import ApplicantList from 'pages/program/AllApplicantPage';
 import StudentApplicant from 'pages/program/StudentApplicant';
-
-import Header from 'components/common/layout/Header';
-import Footer from 'components/common/layout/Footer';
-
-//Import Css
-import 'assets/styles/Header.css';
-import 'assets/styles/Footer.css';
 
 function Router() {
     return(
@@ -46,7 +39,6 @@ function Router() {
                 <Route path='/emp/manageCounseling' element={<ManageCounseling />} />
 {/* EMP파트 END */}
 {/* 프로그램 파트 START */}
-                <Route path='/empHome' element={<EmpHome />} />
                 <Route path='/program' element={<ProgramList />} /> // 프로그램 목록 
                 <Route path='/program/:programNo' element={<ProgramDetail />}/> // 프로그램 상세페이지
                 <Route path='/program/create' element={<ProgramForm />} /> // 프로그램 작성
