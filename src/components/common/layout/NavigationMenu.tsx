6c3529e7d526da976741fb36be5e9af0b42bc2fd
Jamie Yoo <naive0612@gmail.com>
2024-07-17 22:44:00 +0900

    feat : navigation Icon 추가
    네비 아이콘 추가

--- src/components/common/layout/NavigationMenu.tsx
+++ src/components/common/layout/NavigationMenu.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import GroupsIcon from '@mui/icons-material/Groups';
+import ChatIcon from '@mui/icons-material/Chat';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
 //import Css
 import 'assets/styles/common/NavigationMenu.css';
@@ -20,23 +25,33 @@ export default function NavigationMenu() {
                 <div></div>
                 <div className='navigation-menu-grid'>
                     <div className='menu-group-area'>
-                        <div className='move-btn' onClick={() => handleNavBtn('/main')}></div>
+                        <div className='move-btn' onClick={() => handleNavBtn('/main')}>
+                            <HomeIcon />
+                        </div>
                         <div className='btn-text'>My Home</div>
                     </div>
                     <div className='menu-group-area'>
-                        <div className='move-btn' onClick={() => handleNavBtn('/counseling')}></div>
+                        <div className='move-btn' onClick={() => handleNavBtn('/counseling')}>
+                            <PersonIcon />
+                        </div>
                         <div className='btn-text'>1대1 상담</div>
                     </div>
                     <div className='menu-group-area'>
-                        <div className='move-btn' onClick={() => handleNavBtn('/program')}></div>
+                        <div className='move-btn' onClick={() => handleNavBtn('/program')}>
+                            <GroupsIcon />
+                        </div>
                         <div className='btn-text'>상담 프로그램</div>
                     </div>
                     <div className='menu-group-area'>
-                        <div className='move-btn' onClick={() => handleNavBtn('/online')}></div>
+                        <div className='move-btn' onClick={() => handleNavBtn('/online')}>
+                            <ChatIcon />
+                        </div>
                         <div className='btn-text'>온라인 상담</div>
                     </div>
                     <div className='menu-group-area'>
-                        <div className='move-btn' onClick={() => handleNavBtn('/mypage')}></div>
+                        <div className='move-btn' onClick={() => handleNavBtn('/mypage')}>
+                            <ManageAccountsIcon />
+                        </div>
                         <div className='btn-text'>My Page</div>
                         {/* <div className='btn-text'>지도교수 관리(사용자가 교직원인 경우)</div> */}
                     </div>
